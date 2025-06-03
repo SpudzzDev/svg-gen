e190ec3e7a8c6d2fcf329d21722615f5aaef3cf6
SpudzzDev <101953697+SpudzzDev@users.noreply.github.com>
2025-06-03 06:37:01 -0300

    Add files via upload

--- api/generate.js
+++ api/generate.js
@@ -1,13 +1,13 @@
 const { createCanvas } = require('@napi-rs/canvas');
 
-module.exports = async (req, res) => {
+module.exports = (req, res) => {
   const { color = 'purple' } = req.query;
 
   const palettes = {
     purple: ["#2b003e", "#4c007d", "#7a1ca6", "#b86ccf", "#e7b8ff"],
-    blue:   ["#001f3f", "#005f7f", "#008cba", "#7fdbff", "#d0f0ff"],
-    green:  ["#013220", "#026440", "#04a66e", "#68d9a4", "#d1f5e0"],
-    red:    ["#3b0000", "#7f0000", "#a60000", "#cf6c6c", "#ffd1d1"]
+    blue: ["#001f3f", "#005f7f", "#008cba", "#7fdbff", "#d0f0ff"],
+    green: ["#013220", "#026440", "#04a66e", "#68d9a4", "#d1f5e0"],
+    red: ["#3b0000", "#7f0000", "#a60000", "#cf6c6c", "#ffd1d1"]
   };
 
   const waveColors = palettes[color] || palettes.purple;
